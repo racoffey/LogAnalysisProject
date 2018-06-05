d3b4f8448b1e135de474ce8bfe8d6263380f463a
Robert Coffey <racoffey@gmail.com>
2018-06-05 13:03:24 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# Log Analysis Script
+
+This script produces the following 3 reports based on the "Udacity News DB":
+
+1. Top 3 most popular articles
+2. Authors ordered by number of their article views
+3. HTTP error report, showing days with more than 1% errors
+
+It is assumed that the "Udacity News DB" is already installed and running in a PostgreSQL DB.
+
+The script creates the connection to the PostgreSQL DB and runs the required SQL queries outputting the report to the terminal display.
+
+For some of the reports the views are created to support the calculation of the reports. These views are created and cleaned up within the script.
+
+##Example output
+
+The reported output is shown as follows:
+-----------------------------------------------------
+|      Top 3 most popular articles      |   Views   |
+-----------------------------------------------------
+|   Candidate is jerk, alleges rival    |  342102   |
+|   Bears love berries, alleges bear    |  256365   |
+|   Bad things gone, say good people    |  171762   |
+-----------------------------------------------------
+-----------------------------------------------------
+|                Author                 |   Views   |
+-----------------------------------------------------
+|   Ursula La Multa                     |   512805  |
+|   Rudolf von Treppenwitz              |   427781  |
+|   Anonymous Contributor               |   171762  |
+|   Markoff Chaney                      |   85387   |
+-----------------------------------------------------
+-----------------------------------------------------
+|            Date              |   Percent Errors   |
+-----------------------------------------------------
+|         2016-07-17           |        2.26        |
+-----------------------------------------------------
+
+
+## How to use
+The server is written in Python 3.6.5, so Python 3 shall be downloaded and used to run the file log_analysis.py.
+Python 2 can be used but the PostgreSQL library will be required (see troubleshooting guide)
+
+1. Clone the directory
+2. Navigate to the directory cd repository_name
+3. If needed download Python 3 (version 3.6.5 is recommended)
+4. IDLE: Open IDLE
+3. To run the program with the command `python3 log_analysis.py` or `python2 log_analysis.py`
+5. In the menu bar click on Run -> Run Module or press F5 on your keyboard
+
+## Troubleshooting
+If the error "no module named psycopg2" is received, it is likely Python 2.x is being run.  Please upgrade to Python 3.6.5 here:
+https://www.python.org/downloads/ 
+
+Alternatively download the psycopg2 library:
+
+1. From the directory containing the script, install the dependencies:
+`sudo apt-get build-dep python-psycopg2`
+
+2. Then install the library:
+`pip install psycopg2` 
---
+++ log_analysis.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python3
+#
+# The program extracts reports from the "Udacity News DB".
+# The scripts report on the most popular articles, authors and
+# also the HTTP errors reported back to users accessing the articles.
+
+import psycopg2
+
+
+# This method opens the DB connection and requests the three reports.
+def log_analysis():
+    """
+        This method opens the DB connection to the "Udacity News DB"
+        and then requests three reports:
+        1. Top 3 most popular articles
+        2. Authors ordered by number of views
+        3. HTTP error report, showing days with more than 1% errors
+    """
+    # Open connection to "news" DB
+    connection = psycopg2.connect("dbname=news")
+    cursor = connection.cursor()
+
+    # Request three reports
+    get_most_popular_articles(cursor)
+    get_authors_views(cursor)
+    get_error_report(cursor)
+
+    # Close DB connection
+    connection.close()
+
+
+# This method generates a report showing the 3 most popular articles
+def get_most_popular_articles(cursor):
+    """
+        This method gets the 3 most popular articles and prints them
+        together with the number of views.
+
+        Parameters
+        ---------
+        cursor : Cursor
+            DB connections cursor for safely running queries towards the DB.
+    """
+    # Join log and articles table by finding articles.slug in the log.path
+    # string. Use this linkage to find the number of views for each article
+    # and then select the top 3.
+    cursor.execute("""select articles.title, count(*) as views
+                   from log, articles
+                   where log.path like CONCAT('%',articles.slug,'%')
+                   group by articles.title
+                   order by views desc
+                   limit 3;""")
+    results = cursor.fetchall()
+
+    # Print results in a structured format.
+    print("-----------------------------------------------------")
+    print("|      Top 3 most popular articles      |   Views   |")
+    print("-----------------------------------------------------")
+    for item in results:
+        print("|   " + str(item[0]) + "    |  " + str(item[1])) + "   |"
+    print("-----------------------------------------------------")
+
+
+# This methods generates a report showing the authors by number of views
+def get_authors_views(cursor):
+    """
+        This method get the authors and prints them in order of
+        their articles views.
+
+        Parameters
+        ---------
+        cursor : Cursor
+            DB connections cursor for safely running queries towards the DB.
+    """
+
+    # Join log and articles table by finding articles.slug in the log.path
+    # string and also articles and authors table using the author.id.
+    # This linkage is used to find the number of article views by author.
+    # The list of authors ordered by views is then printed.
+    cursor.execute("""select authors.name, count(*) as views
+                   from log, articles, authors
+                   where log.path like CONCAT('%',articles.slug,'%')
+                   and articles.author = authors.id
+                   group by authors.name
+                   order by views desc;""")
+    results = cursor.fetchall()
+
+    # Print results in a structured format.
+    print("-----------------------------------------------------")
+    print("|                Author                 |   Views   |")
+    print("-----------------------------------------------------")
+    for item in results:
+        string = "|                                        |            |"
+        string = string[:4] + item[0] + string[5+len(item[0]):]
+        string = string[:44] + str(item[1]) + string[45+len(str(item[1])):]
+        print(string)
+    print("-----------------------------------------------------")
+
+
+# This method generates a report showing the percentage of HTTP errors
+def get_error_report(cursor):
+    """
+        This method generates a report showing the percentage of HTTP errors
+        for days when over 1% of responses are erroneus
+
+        Parameters
+        ---------
+        cursor : Cursor
+            DB connections cursor for safely running queries towards the DB.
+    """
+
+    # Needed views are created
+    create_successes_view(cursor)
+    create_failures_view(cursor)
+
+    # Views are combined to provide % errors per day.  Days with over 1% errors
+    # are extracted.
+    cursor.execute("""select failures.time as date,
+                   round(CAST(failures.num as decimal)/
+                   CAST((successes.num+failures.num) as decimal)*100, 2)
+                   as percent_errors from successes,failures
+                   where successes.time = failures.time
+                   and round(CAST(failures.num as decimal)/
+                   CAST((successes.num+failures.num) as decimal)*100, 2) > 1;
+""")
+    results = cursor.fetchall()
+
+    # Print results in a structured format.
+    print("-----------------------------------------------------")
+    print("|            Date              |   Percent Errors   |")
+    print("-----------------------------------------------------")
+    for item in results:
+        string = "|                               |                     |"
+        string = string[:10] + str(item[0]) + string[11+len(str(item[0])):]
+        string = string[:40] + str(item[1]) + string[41+len(str(item[1])):]
+        print(string)
+    print("-----------------------------------------------------")
+
+    # Clean up views
+    clean_up(cursor)
+
+
+def create_successes_view(cursor):
+    """Create view with number of successful requests grouped by day"""
+    cursor.execute("""create view successes as
+                   select status, CAST(time As DATE), count(*) as num
+                   from log where status like '2__ OK'
+                   group by status, CAST(time AS DATE)
+                   order by CAST(time AS DATE);""")
+
+
+# Create view with number of failed requests grouped by day
+def create_failures_view(cursor):
+    """Create view with number of failed requests grouped by day"""
+    cursor.execute("""create view failures as
+                   select status, CAST(time As DATE), count(*) as num
+                   from log
+                   where status not like '2__ OK'
+                   group by status, CAST(time AS DATE)
+                   order by CAST(time AS DATE);""")
+
+
+# Clean up views
+def clean_up(cursor):
+    """Delete the view tables to clean up"""
+    cursor.execute("""drop view successes""")
+    cursor.execute("""drop view failures""")
+
+# Run program
+log_analysis()
